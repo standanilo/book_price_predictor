softmax function fix

ml/log_multinom.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 def softmax(z):
-    exp_z = np.exp(z - np.max(z, axis=1, keepdims=True))
+    exp_z = np.exp(z)
     return exp_z / np.sum(exp_z, axis=1, keepdims=True)
 
 class LogisticRegression: